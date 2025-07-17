45dd209d509afa507a896d4876973080ec9943a5
Guilherme Bracale <69833761+Guibracale@users.noreply.github.com>
2025-07-17 13:30:28 -0300

    Add .next/static/chunks/src_lib_auth_ts_4de42ae4._.js 1752770612269

---
+++ .next/static/chunks/src_lib_auth_ts_4de42ae4._.js
@@ -0,0 +1,19 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/src/lib/auth.ts [app-client] (ecmascript, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "static/chunks/node_modules_@firebase_firestore_dist_index_esm2017_c2fcaa2e.js",
+  "static/chunks/node_modules_@firebase_auth_dist_esm2017_d0a1459b._.js",
+  "static/chunks/node_modules_d2a89b4e._.js",
+  "static/chunks/src_lib_fa8f0d76._.js",
+  "static/chunks/src_lib_auth_ts_44c80c87._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/lib/auth.ts [app-client] (ecmascript)");
+    });
+});
+}}),
+}]);
