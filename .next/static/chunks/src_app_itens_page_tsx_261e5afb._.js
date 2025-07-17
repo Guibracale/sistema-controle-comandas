4c52438ecedbe5ea4fd40b1f91546b16383a1b34
Guilherme Bracale <69833761+Guibracale@users.noreply.github.com>
2025-07-17 13:30:20 -0300

    Add .next/static/chunks/src_app_itens_page_tsx_261e5afb._.js 1752770604174

---
+++ .next/static/chunks/src_app_itens_page_tsx_261e5afb._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_2c3861ce._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_@firebase_firestore_dist_index_esm2017_c2fcaa2e.js",
+  "static/chunks/node_modules_@firebase_auth_dist_esm2017_d0a1459b._.js",
+  "static/chunks/node_modules_78d187f3._.js"
+],
+    source: "dynamic"
+});
